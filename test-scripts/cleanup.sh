b4f132710c7b35134b33c9f69f1239c3a627dd4d
Jörg Faulhaber <joerg.fa@gmx.de>
2021-06-10 18:08:03 +0200

    refactoring

--- test-scripts/cleanup.sh
+++ test-scripts/cleanup.sh
@@ -6,4 +6,6 @@ kubectl delete clusterrolebinding kc-monitor-configmaps --ignore-not-found
 
 kubectl delete pod busyboxplus --ignore-not-found
 
+
+
 echo "success -> all resources for kafka-connect-k8s-operator deleted"
